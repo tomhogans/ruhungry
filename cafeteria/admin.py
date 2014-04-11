ed3a320edd419622481b20df0a1a1d7da9eb7467
Tom Hogans <tomhsx@gmail.com>
2014-04-10 21:33:30 -0400

    Define models

--- cafeteria/admin.py
+++ cafeteria/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from ruhungry.models import Station, Food, Order
 
-# Register your models here.
+
+class OrderAdmin(admin.ModelAdmin):
+    #list_display = ('user', '')
+    readonly_fields = ('created_at', 'updated_at')
+
+
+admin.site.register(Station)
+admin.site.register(Food)
+admin.site.register(Order, OrderAdmin)
--- cafeteria/models.py
+++ cafeteria/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Station(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return '%s Station' % self.name
+
+
+class Food(models.Model):
+    station = models.ForeignKey('Station')
+    name = models.CharField(max_length=255, unique=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    available = models.BooleanField(default=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.id
